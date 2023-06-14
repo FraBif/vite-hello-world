88067d874f32f83efbfa56f5e5b7dde88dbbeabc
FraBif <francescobif@hotmail.it>
2023-06-14 17:45:35 +0200

    not visualizing

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+ export default {
+      data() {
+        return {
+            msg: 'Sono veramente euforico! La mia prima App con Vite',
+        }
+      },
+    }
+</script>
+ 
+<template>
+    <h1>
+        {{ msg }}
+    </h1>
+</template>
+
+<style scoped>
+
+h1{
+    color: aquamarine;
+}
+
+</style>
